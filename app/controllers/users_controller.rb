c89cc1f8a177355678e300a3df665586f275d333
Diego Zaizar <diegozaizar26@gmail.com>
2022-03-13 21:01:38 -0600

    users controller created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+    before_action :setup_user, only: %i[ edit show update destroy ]
+    def index
+        @users = User.all
+    end
+
+    def create
+        user = User.new(permitted_params)
+        
+        if user.save
+            flash[:notice] = 'user created!'
+        else
+            flash[:alert] = 'error while creating user'
+        end
+
+        redirect_to users_path
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def edit
+    end
+
+    def show
+    end
+
+    def update
+        if @user.update(permitted_params)
+            flash[:notice] = 'user updated!'
+        else
+            flash[:noticalerte] = 'error while updating a user'
+        end
+
+        redirect_to user_path
+    end
+
+    def destroy
+        if @user.destroy
+            flash[:notice] = 'user deleted!'
+        else
+            flash[:alert] = 'error while deleting a user'
+        end
+        
+        redirect_to users_path
+    end
+
+    private 
+    def permitted_params
+        params.require(:user).permit(:name, :age)
+    end
+
+    def setup_user
+        @user =  User.find(params[:id])
+    end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:diego)
+  end
+
+  test 'should have index' do
+    get users_path
+
+    assert_response :success
+  end
+ 
+  test 'should create a user' do
+    post users_path, params: { user: { name: 'Diego', age: 22 } }
+
+    assert_redirected_to users_path
+    assert_equal 'user created!', flash[:notice]
+  end
+
+  test 'should have new' do
+    get new_user_path
+
+    assert_response :success
+  end
+
+  test 'should have edit' do
+    get edit_user_path(@user)
+
+    assert_response :success
+  end
+
+  test 'should show a user' do
+    get user_path(@user)
+
+    assert_response :success
+  end
+
+  test 'should update a user' do
+    put user_path(@user), params: { user: { name: 'Carlos', age: 19 } }
+
+    assert_redirected_to user_path
+    assert_equal 'user updated!', flash[:notice]
+  end
+
+  test 'should destroy a user' do
+    delete user_path(@user)
+
+    assert_redirected_to users_path
+    assert_equal 'user deleted!', flash[:notice]
+  end
+end
