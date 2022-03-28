0eab67aaca1d2fb7e28cc197bf48ae32268275c8
Diego Zaizar <diegozaizar26@gmail.com>
2022-03-28 13:59:19 -0600

    Change indentation to 2 spaces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,76 +1,73 @@
 require 'csv'
 
 class UsersController < ApplicationController
-    FILE_HEADERS = ['Name', 'Age'].freeze
-    before_action :setup_user, only: %i[ edit show update destroy ]
+	FILE_HEADERS = ['Name', 'Age'].freeze
+	before_action :setup_user, only: %i[ edit show update destroy ]
 
-    def index
-        @users = User.all
-        respond_to do |format|
-            format.html
-            format.csv do
-                file = CSV.generate(headers: true) do |csv|
-                    csv << FILE_HEADERS
-                    @users.each do |user|
-                        csv << [user.name, user.age]
-                    end
-                  end
-                send_data file, filename: "users #{ Date.strptime(Date.today.to_s, '%Y-%m-%d') }.csv" 
-            end
-        end
-    end
+	def index
+		@users = User.all
+		respond_to do |format|
+			format.html
+			format.csv do
+				file = CSV.generate(headers: true) do |csv|
+					csv << FILE_HEADERS
+					@users.each do |user|
+						csv << [user.name, user.age]
+					end
+				end
+				send_data file, filename: "users #{ Date.strptime(Date.today.to_s, '%Y-%m-%d') }.csv" 
+			end
+		end
+	end
 
-    def create
-        user = User.new(permitted_params)
-        
-        if user.save
-            flash[:notice] = 'user created!'
-            redirect_to users_path
-        else
-            flash.now[:alert] = 'error while creating user'
-            render 'new'
-        end
+	def create
+		user = User.new(permitted_params)
+		
+		if user.save
+			flash[:notice] = 'user created!'
+			redirect_to users_path
+		else
+			flash.now[:alert] = 'error while creating user'
+			render 'new'
+		end
+	end
 
-    end
+	def new
+		@user = User.new
+	end
 
-    def new
-        @user = User.new
-    end
+	def edit
+	end
 
-    def edit
-    end
+	def show
+	end
 
-    def show
-    end
+	def update
+		if @user.update(permitted_params)
+			flash[:notice] = 'user updated!'
+			redirect_to users_path
+		else
+			flash.now[:alert] = 'error while updating a user'
+			render :edit
+		end
+	end
 
-    def update
-        if @user.update(permitted_params)
-            flash[:notice] = 'user updated!'
-            redirect_to users_path
-        else
-            flash.now[:alert] = 'error while updating a user'
-            render :edit
-        end
+	def destroy
+		if @user.destroy
+			flash[:notice] = 'user deleted!'
+		else
+			flash[:alert] = 'error while deleting a user'
+		end
+		redirect_to users_path
+	end
 
-    end
+	private
+	
+	def permitted_params
+		params.require(:user).permit(:name, :age)
+	end
 
-    def destroy
-        if @user.destroy
-            flash[:notice] = 'user deleted!'
-        else
-            flash[:alert] = 'error while deleting a user'
-        end
-        
-        redirect_to users_path
-    end
-
-    private
-    
-    def permitted_params
-        params.require(:user).permit(:name, :age)
-    end
-
-    def setup_user
-        @user =  User.find(params[:id])
-    end
+	def setup_user
+		@user =  User.find(params[:id])
+	end
 end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,77 +1,76 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
+	test 'visiting the index' do
+		visit users_path
 
-    test 'visiting the index' do
-        visit users_path
+		assert_content 'Authors'
+	end
 
-        assert_content 'Authors'
-    end
+	test 'create a user' do
+		visit users_path
+		click_on 'Create'
 
-    test 'create a user' do
-        visit users_path
-        click_on 'Create'
+		fill_in 'Name', with: 'Oscar'
+		fill_in 'Age', with: 19
+		click_on 'Create User'
 
-        fill_in 'Name', with: 'Oscar'
-        fill_in 'Age', with: 19
-        click_on 'Create User'
+		assert_content 'user created!'
+	end
 
-        assert_content 'user created!'
-    end
+	test 'create a user with incorrect values' do
+		visit users_path
+		click_on 'Create'
 
-    test 'create a user with incorrect values' do
-        visit users_path
-        click_on 'Create'
+		fill_in 'Name', with: ''
+		fill_in 'Age', with: 19
+		click_on 'Create User'
 
-        fill_in 'Name', with: ''
-        fill_in 'Age', with: 19
-        click_on 'Create User'
+		assert_content 'error while creating user'
+	end
 
-        assert_content 'error while creating user'
-    end
+	test 'show a user' do
+		visit users_path
+		user = users(:diego)
 
-    test 'show a user' do
-        visit users_path
-        user = users(:diego)
+		click_on 'Show', match: :first
 
-        click_on 'Show', match: :first
+		assert_content user.name
+	end
 
-        assert_content user.name
-    end
+	test 'Edit a user' do
+		visit users_path
+		user = users(:diego)
+		
+		click_on 'Edit'
 
-    test 'Edit a user' do
-        visit users_path
-        user = users(:diego)
-        
-        click_on 'Edit'
+		fill_in 'Name', with: 'Diego'
+		fill_in 'Age', with: 12
+		click_on 'Edit User'
 
-        fill_in 'Name', with: 'Diego'
-        fill_in 'Age', with: 12
-        click_on 'Edit User'
+		assert_content user.name
+	end
 
-        assert_content user.name
-    end
+	test 'Edit a user with incorrect values' do
+		visit users_path
+		user = users(:diego)
+		
+		click_on 'Edit'
 
-    test 'Edit a user with incorrect values' do
-        visit users_path
-        user = users(:diego)
-        
-        click_on 'Edit'
+		fill_in 'Name', with: ''
+		fill_in 'Age', with: 12
+		click_on 'Edit User'
 
-        fill_in 'Name', with: ''
-        fill_in 'Age', with: 12
-        click_on 'Edit User'
+		assert_content 'error while updating a user'
+	end
 
-        assert_content 'error while updating a user'
-    end
+	test 'destroy a user' do
+		visit users_path
 
-    test 'destroy a user' do
-        visit users_path
+		page.accept_alert 'Are u sure?' do
+				click_on 'Destroy'
+		end
 
-        page.accept_alert 'Are u sure?' do
-            click_on 'Destroy'
-        end
-
-        assert_content 'user deleted!'
-    end
+		assert_content 'user deleted!'
+	end
 end
