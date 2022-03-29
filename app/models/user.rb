da02251a2668ae815a0dd6b34ff5f500707de6fd
Diezagu <42851283+Diezagu@users.noreply.github.com>
2022-03-29 08:58:14 -0600

    Change indentation to 2 spaces (#7)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :books, foreign_key: 'author_id'
-    validates :name, :age, presence: true
-    validates :age, numericality: { greater_than: 0, less_than: 100 }
+	has_many :books, foreign_key: 'author_id'
+	validates :name, :age, presence: true
+	validates :age, numericality: { greater_than: 0, less_than: 100 }
 end
