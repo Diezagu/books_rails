cbff4acf7203edcc54a98879b447c563134d0364
Diezagu <42851283+Diezagu@users.noreply.github.com>
2022-04-20 07:40:08 -0500

    Add avatar or profile picture to users (#24)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, foreign_key: 'author_id'
+  has_one_attached :avatar
+
   validates :name, :age, presence: true, allow_blank: false
   validates :age, numericality: { greater_than: 0, less_than: 100 }
+  validates :avatar, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 end
