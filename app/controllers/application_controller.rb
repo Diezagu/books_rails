98158908985980a4a6834b26b4ab914e7125d456
Diego Zaizar <diegozaizar26@gmail.com>
2022-04-19 21:54:23 -0500

    Add avatar or profile picture to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name age])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name age])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name age avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name age avatar])
   end
 end
