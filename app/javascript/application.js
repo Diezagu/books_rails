5f72211169c055afb2a81ef83b09fc53af0b898b
Diego Zaizar <diegozaizar26@gmail.com>
2022-04-28 11:46:03 -0500

    Change trix and action text to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,5 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("./channels")
+require("trix")
+require("@rails/actiontext")
 
 import './stylesheets/application.scss'
