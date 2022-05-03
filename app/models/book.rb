ebf551617c5e5bc7dd7025337a434e616ab6ce74
Diego Zaizar <diegozaizar26@gmail.com>
2022-05-03 14:32:32 -0500

    Add deleted line accidentaly

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@
 class Book < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_one_attached :cover
+  has_many :comments
   has_rich_text :synopsis
 
   validates :title, :author, :pages, presence: true
